3301b6b4de933a82e23c9a65ca62684ff2b30784
matiasua <matiasncp7@gmail.com>
2020-09-05 00:33:26 -0400

    First Commit

---
+++ docs/precache-manifest.e7e787121ace08e760275a9341c37e95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "078a979c4f74459ddabd48242a813ac6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eed641ebf80a974183f8",
+    "url": "/static/css/main.cd5f4b83.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eed641ebf80a974183f8",
+    "url": "/static/js/main.f5e16207.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
